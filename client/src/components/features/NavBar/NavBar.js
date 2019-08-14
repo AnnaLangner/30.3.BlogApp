4d98d0dee47163c52ab01c7551992e18fe2484cb
Anna Langner <annalangner06@gmail.com>
2019-08-14 15:10:44 +0200

    Adding Links to NavBar

--- client/src/components/features/NavBar/NavBar.js
+++ client/src/components/features/NavBar/NavBar.js
@@ -6,9 +6,11 @@ class NavBar extends React.Component {
     render() {
         return (
             <div>
-                <Link to='/'>Home</Link>
-                <Link to='/posts'>Posts</Link>
-                <Link to='/contact'>Contact</Link>
+                <Link to='/'>Home </Link>
+                <Link to='/posts'>Posts </Link>
+                <Link to='/contact'>Contact </Link>
+                <Link to='/posts/:id'>SinglePost </Link>
+                <Link to='/posts/new'>NewPost </Link>
             </div>
         );
     }
