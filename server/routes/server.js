9f2da2dfffd232677baa5234c03f01ece3c0d290
Anna Langner <annalangner06@gmail.com>
2019-08-26 13:36:23 +0200

    Refactoring
    -Modification of the server directory
    -Modification of the server.js file
    -Adding config.js file
    -Extracting endpoints to the routes folder
    -Import routes to server.js

---
+++ server/routes/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const config = require('./config');
+
+const app = express();
+
+//import routes
+const postRoutes = require('./routes/post.routes');
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use('/api', postRoutes);
+
+app.listen(config.PORT, function () {
+    console.log('Server is running on port: ', config.PORT);
+});
