775721087cbaeefb70b7f9696b4d2561646791cd
Anna Langner <annalangner06@gmail.com>
2019-09-02 13:27:11 +0200

    Adding React-medium-editor

--- client/src/components/features/PostForm/PostForm.js
+++ client/src/components/features/PostForm/PostForm.js
@@ -5,6 +5,9 @@ import React from 'react';
 import TextField from '../../common/TextField/TextField';
 import SectionTitle from '../../common/SectionTitle/SectionTitle';
 import Button from '../../common/Button/Button';
+import Editor from 'react-medium-editor';
+import 'medium-editor/dist/css/medium-editor.css';
+import 'medium-editor/dist/css/themes/default.css';
 
 class PostForm extends React.Component {
     state = {
@@ -23,10 +26,15 @@ class PostForm extends React.Component {
                 <TextField label={'Title'} value={post.title}/>
                 <TextField label={'Author'} value={post.author}/>
                 <SectionTitle>Edit post content</SectionTitle>
+                <Editor
+                    className="content-editor"
+                    text={post.content}
+                    options={{ placeholder: false, toolbar: { buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3'] } }}
+                />
                 <Button variant={'primary'}>Add post</Button>
             </div>
         );
     }
-};
+}
 
 export default PostForm;
