fe0415a4edbfb2e62bec2258532519f7da959d49
Anna Langner <annalangner06@gmail.com>
2019-09-02 14:35:27 +0200

    Swap "Single Post" position with "New Post"

--- client/src/App.js
+++ client/src/App.js
@@ -17,8 +17,8 @@ class App extends React.Component {
                     <Route path="/" exact component={Home} />
                     <Route path="/posts" exact component={Posts} />
                     <Route path="/contact" exact component={Contact} />
-                    <Route path="/posts/:id" exact component={SinglePost} />
                     <Route path="/posts/new" exact component={NewPost} />
+                    <Route path="/posts/:id" exact component={SinglePost} />
                     <Route component={NotFound} />
                 </Switch>
             </MainLayout>
