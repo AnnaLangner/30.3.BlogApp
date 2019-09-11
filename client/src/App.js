f134417a8a1ca74e20afa999bd896c51d34fbef9
Anna Langner <annalangner06@gmail.com>
2019-09-11 12:57:48 +0200

    Adding a Route

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/pages/Home/HomePage';
 import Posts from './components/pages/Posts/PostsPage';
 import Contact from './components/pages/Contact/ContactPage';
 import NotFound from './components/pages/NotFound/NotFoundPage';
+import RandomPost from './components/pages/RandomPost/RandomPostPage'
 import SinglePost from './components/pages/SinglePost/SinglePostPage.js';
 import NewPost from './components/pages/NewPost/NewPost.js';
 
@@ -18,6 +19,7 @@ class App extends React.Component {
                     <Route path="/posts" exact component={Posts} />
                     <Route path="/contact" exact component={Contact} />
                     <Route path="/posts/new" exact component={NewPost} />
+                    <Route path="/post/random" exact component={RandomPost} />
                     <Route path="/posts/:id" exact component={SinglePost} />
                     <Route component={NotFound} />
                 </Switch>
