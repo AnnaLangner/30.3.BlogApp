944c8e4cc3f837fe55f706bdb581cd8b0a15066c
Anna Langner <annalangner06@gmail.com>
2019-09-17 08:51:26 +0200

    Remove "# production /build" from .gitignore

---
+++ client/build/precache-manifest.2c76877e07bbf223030f8997a39c42ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5113cb4ee30f9c90dc1f68105da5f83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "653dc35f436df389db3f",
+    "url": "/static/css/2.ee498ddd.chunk.css"
+  },
+  {
+    "revision": "f3f56c78ff4137c69bc1",
+    "url": "/static/css/main.bd47bc12.chunk.css"
+  },
+  {
+    "revision": "653dc35f436df389db3f",
+    "url": "/static/js/2.ac38bb03.chunk.js"
+  },
+  {
+    "revision": "f3f56c78ff4137c69bc1",
+    "url": "/static/js/main.62015911.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  }
+]);
