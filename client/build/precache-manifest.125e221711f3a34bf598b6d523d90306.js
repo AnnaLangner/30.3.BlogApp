04c4593ec621d99cf949b6576d7f87a53b3daa6b
Anna Langner <annalangner06@gmail.com>
2019-09-20 15:40:54 +0200

    A new role of server

---
+++ client/build/precache-manifest.125e221711f3a34bf598b6d523d90306.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf7111360e4f95b00e64aa1fd139c85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b824288d39ea89910cb",
+    "url": "/static/css/2.ee498ddd.chunk.css"
+  },
+  {
+    "revision": "b969d963955cc79434fc",
+    "url": "/static/css/main.0eb09a23.chunk.css"
+  },
+  {
+    "revision": "9b824288d39ea89910cb",
+    "url": "/static/js/2.a59b7523.chunk.js"
+  },
+  {
+    "revision": "b969d963955cc79434fc",
+    "url": "/static/js/main.3457e600.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  }
+]);
